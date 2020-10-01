2795f51f515e6f8c65fbd0ccfcbb47798242553c
Ajaybab <70493548+Ajaybab@users.noreply.github.com>
2020-10-01 17:02:37 -0500

    Update OddOrEven.java

--- Documents/NetBeansProjects/New Folder/NewPrime/src/main/java/OddOrEven.java
+++ Documents/NetBeansProjects/New Folder/NewPrime/src/main/java/OddOrEven.java
@@ -12,17 +12,38 @@ import java.util.Scanner;
  * @author AJ
  */
 public class OddOrEven {
-    public static void main(String[] args) {
-        int AJ;
-        Scanner Num = new Scanner(System.in);
-        System.out.println("Enter a new number: ");
-        AJ = Num.nextInt();
-        if(AJ % 2 == 0){
-            System.out.println("Number is even");
-        } else{
-            System.out.println("Number is odd");
-           
-        }}
-            
-    
+ 	public static void isEven(int number){ 
+	if((number%2)==0)
+	{
+	System.out.println("The number is Even number");
+	}
+	else
+	{
+	System.out.println("The number is Odd number");
+	}
+	}
+	public static void main(String args[])
+	{
+	int n,Evencount=0,Oddcount=0,num=0;
+	System.out.println("Enter any number: ");
+	Scanner sc = new Scanner(System.in);
+	num = sc.nextInt();
+	isEven(num); 
+	for(n=1;n<=100;n++) 
+	{
+	if(n%2==0)//
+	{
+	Evencount++;
+	  
+	}
+	else
+	{
+	Oddcount++;
+
+	}
+	}
+
+	System.out.println("The Total number of Even Numbers is: "+Evencount);
+	System.out.println("The Total number of Odd Numbers is: "+Oddcount);
+	}
 }
